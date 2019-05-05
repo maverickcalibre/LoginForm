6105a3c350f79c8025dcaec55b90579e7cd1f663
Shaikh, Noor Mohammed <ShaikhN@autonation.com>
2019-05-05 10:29:01 -0400

    Form Validator function created and initial validation of email field

--- LoginRegistration.js
+++ LoginRegistration.js
@@ -491,6 +491,125 @@ AN.FormValidatorMethods = (function(_){
 
 })(AN.FormValidationConstantsModule);
 
+AN.FormValidator = (function(methods, _){
+  var FormValidator = function () {
+        this.form = null;
+        this.formElements = null;
+        this.customValidations = [];
+        this.asyncCustomValidations = [];
+
+        if (arguments && arguments.length >= 1) {
+            this.init(arguments[0], arguments[1] || {});
+        } else {
+            throw 'error: invalid arguments. Please provide at least 1 argument to specify the form context';
+        }
+    };
+
+    FormValidator.prototype = {
+
+        /**
+         * Gets all the input elements of the given form and initializes the eventListeners on them
+         * @param {object} form
+         * @param {object} options By default formValidator runs the input validation on blur and/or on change. These
+         * options allow to specify if any of these actions are disabled or not through "validateOnBlur" and "validateOnChange"
+         * options.
+         */
+        init: function (form, options) {
+            this.form = form;
+
+            options = options || {};
+            options.validateOnBlur = (typeof options.validateOnBlur === "undefined") || options.validateOnBlur;
+            options.validateOnChange = (typeof options.validateOnChange === "undefined") || options.validateOnChange;
+
+            if (!this.form) {
+                throw _.NAME + ' is not loaded!';
+            } else {
+                this.formElements = this.form.querySelectorAll(_.INPUT_SELECTOR);
+                methods.loadEvents(this, options);
+            }
+
+        },
+
+        /**
+         * Runs validations for the given form
+         * @param {object} targetEvent
+         * @returns {boolean} Returns true if all the rules passed, and false otherwise 
+         */
+        validateForm: function () {
+            return methods.validateForm(this);
+        },
+
+        /**
+         * Runs validation for the given input
+         * @param {object} input
+         */
+        validateInput: function (input) {
+            return methods.validateInput(input, this);
+        },
+
+        /**
+         * Adds a custom validation rule to a given input with custom validation logic
+         * 
+         * @param {object} sourceInput - input to validate
+         * @param {object} validationMethod - method to validate
+         * @param {string} triggerName - string with the name of the native event
+         * @param {string} errorMessage - default error message to use case callback returns only true or false
+         * @param {object} destinationInput - input where the error message should be displayed
+         */
+        addCustomValidation: function (sourceInput, validationMethod, triggerName, errorMessage, destinationInput) {
+            var customValidation = methods.addCustomValidation(sourceInput, validationMethod, triggerName, errorMessage, destinationInput);
+            if (customValidation) {
+                this.customValidations.push(customValidation);
+            }
+        },
+
+        /**
+        * Remove custom validation for the given input with custom validation logic
+        * @param {object} sourceInput - input to validate
+        * @param {string} triggerEvent - string with the name of the native event
+        * @param {string} eventHandler - string with the function assign to 'triggerEvent' event
+        * @param {string} errorMessage - default error message to use case callback returns only true or false
+        * @param {object} destinationInput - input where the error message should be displayed
+        */
+        removeCustomValidation: function (sourceInput, triggerEvent, eventHandler, errorMessage, destinationInput) {
+            methods.removeCustomValidation(sourceInput, triggerEvent, eventHandler);
+        },
+
+        addAsyncCustomValidation: function (sourceInput, validationMethod, responseParser, successCallback, failureCallback) {
+            var asyncCustomValidation = methods.addAsyncCustomValidation(sourceInput, validationMethod, responseParser, successCallback, failureCallback);
+            if (asyncCustomValidation) {
+                this.asyncCustomValidations.push(asyncCustomValidation);
+            }
+        },
+
+        /**
+        * Remove validations for the given form
+        * @param {object} targetEvent
+        */
+        removeValidations: function () {
+            return methods.removeValidations(this);
+        },
+
+        /**
+        * Set state to a given element
+        * @param {object} input
+        * @param {string} state
+        */
+        setValidationState: function (input, state) {
+            methods.setValidationState(input, state);
+        },
+
+        /**
+        * Get available validation states of an input or form
+        */
+        validationStates: _.STATES
+
+    };
+
+    return FormValidator;  
+
+})(AN.FormValidatorMethods, AN.FormValidationConstantsModule);
+
 document.addEventListener('DOMContentLoaded', function () {
 
   AN.AuthenticationModule = AN.AuthenticationModule || (AN.AuthenticationModule = {});
@@ -521,60 +640,65 @@ document.addEventListener('DOMContentLoaded', function () {
     };
   })();
 
-  AN.ValidationModule = (function () {    
+  AN.ValidationModule = (function (FormValidator) {
+    
+    var form = document.getElementById('loginForm');
+    this.validator = new FormValidator(form);
+    
     var loginEmail = document.getElementById("login_email");
-    var invalidMsgToShowField = document.getElementById('invalidEmail');
-    //var isEmailDirty = false;    
+        
 
+    self = this;
     loginEmail.addEventListener('invalid', function (event) {
-      event.preventDefault();
-      if (!event.target.validity.valid) {
-        //console.log(event.target.validity);        
-        setValidationMessage(event.target, invalidMsgToShowField);
-        loginEmail.classList.add('invalid');
-      }
+      self.validator.validateInput(this);     
     });
 
+    loginEmail.addEventListener('input', function (event) {
+      self.validator.validateInput(this);     
+    });
+    //#region 
     // loginEmail.addEventListener('focus', function (event) {
     //   isEmailDirty = true;
     // });
 
-    loginEmail.addEventListener('input', function (event) {
-      console.log("loginEmail.value: ", loginEmail.value);
-      console.log("Validity: ", event.target.validity.valid);
-      if (loginEmail.validity.valid) {
-        loginEmail.classList.remove('invalid');
-        invalidMsgToShowField.style.display = 'none';
-      } else {
-        //if (isEmailDirty) {
-          loginEmail.classList.add('invalid');
-          invalidMsgToShowField.style.display = 'block';
-          setValidationMessage(event.target, invalidMsgToShowField);
-        //}
-      }
-    });
+    // loginEmail.addEventListener('input', function (event) {
+    //   console.log("loginEmail.value: ", loginEmail.value);
+    //   console.log("Validity: ", event.target.validity.valid);
+    //   if (loginEmail.validity.valid) {
+    //     loginEmail.classList.remove('invalid');
+    //     invalidMsgToShowField.style.display = 'none';
+    //   } else {
+    //     //if (isEmailDirty) {
+    //       loginEmail.classList.add('invalid');
+    //       invalidMsgToShowField.style.display = 'block';
+    //       setValidationMessage(event.target, invalidMsgToShowField);
+    //     //}
+    //   }
+    // });
 
-    var setValidationMessage = function (ctrl, ctrlToShow){
-        //console.log('$$$ ctrl, ctrlToShow: ', ctrl, ctrlToShow);
-        console.log('$$$ ctrl.validity.patternMismatch: ', ctrl.validity.patternMismatch);
-        switch (true) {
-          case ctrl.validity.valueMissing:
-              //console.log('value is missing');
-              ctrlToShow.textContent = ctrl.dataset.error;
-              ctrlToShow.classList.add('error');
-              ctrlToShow.style.display = 'block';
-            break;
-          case ctrl.validity.patternMismatch:         
-              ctrlToShow.textContent = ctrl.dataset.patternError;
-            break;
+    // var setValidationMessage = function (ctrl, ctrlToShow){
+    //     //console.log('$$$ ctrl, ctrlToShow: ', ctrl, ctrlToShow);
+    //     console.log('$$$ ctrl.validity.patternMismatch: ', ctrl.validity.patternMismatch);
+    //     switch (true) {
+    //       case ctrl.validity.valueMissing:
+    //           //console.log('value is missing');
+    //           ctrlToShow.textContent = ctrl.dataset.error;
+    //           ctrlToShow.classList.add('error');
+    //           ctrlToShow.style.display = 'block';
+    //         break;
+    //       case ctrl.validity.patternMismatch:         
+    //           ctrlToShow.textContent = ctrl.dataset.patternError;
+    //         break;
           
-          default:
-            break;
-        }
+    //       default:
+    //         break;
+    //     }
 
-        //console.log('$$$ ctrlToShow: ', ctrlToShow);
-    };
+    //     //console.log('$$$ ctrlToShow: ', ctrlToShow);
+    // };
 
-  }());
+    //#endregion
+
+  }(AN.FormValidator));
 
 });
